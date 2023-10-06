8d0f349848ed59439d6329e068c188549d86f1ba
Aditya Goyal <adityamr.1694@gmail.com>
2023-10-06 23:04:07 +0530

    Create Lecture5Part2.cpp

---
+++ Lecture5Part2.cpp
@@ -0,0 +1,39 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+// 	int a[5];
+// 	cout<<"Enter 5 integers:"<<endl;
+	
+// 	for(int i=0;i<5;i++){
+// 		cin>>a[i];
+// 	}
+	
+// 	cout<<"The 5 given inputs are: "<<endl;
+	
+// 	for(int i=0;i<5;i++){
+// 		cout<<a[i];
+// 	}
+// 	return 0;
+// }
+
+//print the output in reverse order using array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a[10];
+	cout<<"Enter 10 integers:"<<endl;
+	
+	for(int i=0;i<10;i++){
+		cin>>a[i];
+	}
+	
+	cout<<"The 10 given inputs in reverse order are: "<<endl;
+	
+	for(int i=9;i>=0;i--){
+		cout<<a[i];
+	}
+	return 0;
+}
